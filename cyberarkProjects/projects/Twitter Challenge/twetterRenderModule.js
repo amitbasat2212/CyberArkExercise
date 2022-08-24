16cb903ee80de29fcba87112ec360047c9ac5b4d
amit ben basat <amitbasat124@gmail.com>
2022-08-24 09:13:27 +0300

    adding al the twetter project

--- cyberarkProjects/projects/Twitter Challenge/twetterRenderModule.js
+++ cyberarkProjects/projects/Twitter Challenge/twetterRenderModule.js
@@ -3,14 +3,18 @@ const TwetterRenderModule=()=>{
         $('#posts').empty();
         for(let i=0;i<posts.length;i++){
             let Post = `<div class="allPost">
-            <div id=${posts[i].id} class="postsPerPost">
-            <div class="posttext"  data-id=${posts[i].id} >${posts[i].text} :</div>
+            <div id=${posts[i].id}  data-id=${posts[i].id} class="postsPerPost">
+            <div class="posttext">${posts[i].text} :</div>
+            <button class="delete">delete</button>
+            <button class="CommentButton">Add comment</button>
+            <input class="textfornewComment" placeholder="write me yout thoghts">
             </div>
             </div>`
             $('#posts').append(Post)
             for(let j=0;j<posts[i].comments.length;j++){
-                let comment = `<div class="comments>
+                let comment = `<div class="comments">
                 <div class="commentText" data-id=${posts[i].comments[j].id}>${posts[i].comments[j].text}:</div>
+                <button class="delete-comment">delete</button>
                 </div>`
                 $(`#${posts[i].id}`).append(comment)
             }            
